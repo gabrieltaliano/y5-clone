01c9e383d285f24b9a0133b4c5c9cc5405c93349
Gabriel Taliano <gabrieltaliano@gmail.com>
2023-08-01 07:57:02 +1200

    Rename project

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
